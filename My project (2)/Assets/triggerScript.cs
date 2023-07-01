44ad0eb1e9ce11f960a661498b607dd585576995
TheFroman <Vertonghengod@gmail.com>
2023-07-02 01:23:51 +0200

    Added Pawn
    Added pawn from this free source: https://free3d.com/3d-model/woodenchesspawnsideb-v2--259796.html

--- My project (2)/Assets/triggerScript.cs
+++ My project (2)/Assets/triggerScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class triggerScript : MonoBehaviour
+public class TriggerScript : MonoBehaviour
 {
     public ScoreManagerScript scoreManager;
     // Start is called before the first frame update
@@ -18,6 +18,8 @@ public class triggerScript : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.layer = 3) { 
         scoreManager.addScore();
+    }
     }
 }
