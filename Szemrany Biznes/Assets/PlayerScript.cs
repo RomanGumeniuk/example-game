4d41bcc85baf1e6c0fa8e799314dbad2c83ec488
WisniewskiOlaf <olwis198@wp.pl>
2023-07-02 01:06:14 +0200

    someStuff

--- Szemrany Biznes/Assets/PlayerScript.cs
+++ Szemrany Biznes/Assets/PlayerScript.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class PlayerScript : MonoBehaviour
+public class PlayerScript : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - (1 * Time.deltaTime), transform.position.y, transform.position.z);
+        }
     }
 }
