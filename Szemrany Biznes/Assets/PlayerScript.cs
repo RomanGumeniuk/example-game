28fde6052c98a5abab9885cf47ed0b48c4e04ee1
WisniewskiOlaf <olwis198@wp.pl>
2023-07-02 12:51:59 +0200

    UI and board

--- Szemrany Biznes/Assets/PlayerScript.cs
+++ Szemrany Biznes/Assets/PlayerScript.cs
@@ -5,9 +5,15 @@ using Unity.Netcode;
 
 public class PlayerScript : NetworkBehaviour
 {
+    private void Start()
+    {
+        transform.position = new Vector3(-4.25f, transform.position.y, -4.25f);
+    }
+
+
     private void Update()
     {
-        
+        if (!IsOwner) return;
         if(Input.GetKey(KeyCode.A))
         {
             transform.position = new Vector3(transform.position.x - (1 * Time.deltaTime), transform.position.y, transform.position.z);
