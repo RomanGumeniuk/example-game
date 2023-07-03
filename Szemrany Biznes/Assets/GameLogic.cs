80b9d1aea0a17b6a62af031a6875efc120965b02
WisniewskiOlaf <olwis198@wp.pl>
2023-07-03 23:24:30 +0200

    Revert "Merge branch 'dominosBranch' into moje2"
    This reverts commit d36a1cb11ea1d3399422f20fc048a302c6790b2f, reversing
    changes made to 2cdb4298d283b9c64ad236ec973987dbe9769353.

--- Szemrany Biznes/Assets/GameLogic.cs
+++ Szemrany Biznes/Assets/GameLogic.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.Events;
+using TMPro;
 
 public class GameLogic : NetworkBehaviour
 {
@@ -15,6 +16,10 @@ public class GameLogic : NetworkBehaviour
 
     public int index = 0;
     public int allPlayerAmount = 0;
+
+    public Transform content;
+    public GameObject playerListPrefab;
+
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
@@ -78,6 +83,9 @@ public class GameLogic : NetworkBehaviour
         {
             int currentIndex = Random.Range(0, allPlayerAmount - i);
             PlayersOrder.Add(NetworkManager.Singleton.ConnectedClients.GetValueOrDefault<ulong, NetworkClient>(allPlayerIndex[currentIndex]));            allPlayerIndex.RemoveAt(currentIndex);
+            GameObject playerPrefabList = Instantiate(playerListPrefab, content);
+            playerListPrefab.transform.Find("Name").GetComponent<TextMeshProUGUI>().text = "Player#" + currentIndex.ToString();
+            playerListPrefab.transform.Find("Money").GetComponent<TextMeshProUGUI>().text = "3000PLN";
         }
         index = 0;
         OnNextPlayerTurnServerRpc();
@@ -109,7 +117,11 @@ public class GameLogic : NetworkBehaviour
         ClientHasPermissionToRollDiceClientRpc(clientRpcParams);
     }
 
-    
+    [ServerRpc(RequireOwnership =false)]
+    public void GivePlayerMoneyByIdServerRpc(int playerId,int money)
+    {
+        NetworkManager.Singleton.ConnectedClients[(ulong)playerId].PlayerObject.GetComponent<PlayerScript>().amountOfMoney += money;
+    }
 
     
 }
