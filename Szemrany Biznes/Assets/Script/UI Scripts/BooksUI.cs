8c9b1efee6d658cc744cdf6f0c67f681ba2182fb
rta228 <vectro@onet.pl>
2025-02-16 00:08:35 +0100

    nyga

--- Szemrany Biznes/Assets/Script/UI Scripts/BooksUI.cs
+++ Szemrany Biznes/Assets/Script/UI Scripts/BooksUI.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
 
 public class BooksUI : MonoBehaviour
 {
-    [SerializeField] private RawImage[] bookImages; // Lista obrazków
+    private RaycastHit2D raycastHit2D;
+    [SerializeField] private Transform[] bookImages; // Lista obrazków
+    [SerializeField] private Texture BlackSlide;
+    [SerializeField] private Texture RedSlide;
 
     [Header("Guide UI")]
     [SerializeField] private Image GuideUIBackground;
@@ -12,14 +17,48 @@ public class BooksUI : MonoBehaviour
     [SerializeField] private Image GuideUIBookmarks;
     [SerializeField] private Image GuideUIClose;
 
-    private void Start()
+    public void Start()
     {
-        foreach (RawImage img in bookImages)
+        StartCoroutine(CheckMouseOverUI());
+    }
+
+    public void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
         {
-            img.gameObject.AddComponent<ImageSegment>(); // Dodanie skryptu do ka¿dego obrazka
+            foreach (Transform child in bookImages)
+            {
+                if (child.GetComponent<RawImage>().color == Color.red) {
+                    Debug.Log("Clicked" + child.name);
+                    //TUTAJ WPEIRDOL DOJENAY KOD 
+                };
+            }
         }
     }
 
+    IEnumerator CheckMouseOverUI()
+    {
+        while (true)
+        {
+            yield return 0;
+            raycastHit2D = Physics2D.Raycast(Input.mousePosition, Vector2.zero);
+            foreach (Transform child in bookImages)
+            {
+                if (raycastHit2D.collider?.name == child.name)
+                {
+                    child.GetComponent<RawImage>().color = Color.red;
+                    child.GetComponent<RawImage>().texture = RedSlide;
+                    Debug.Log(child.name);
+
+                }
+                else
+                {
+                    child.GetComponent<RawImage>().color = Color.black;
+                    child.GetComponent<RawImage>().texture = BlackSlide;
+                }
+            }
+        }
+    }
     public void OnBookClicked()
     {
         GuideUIBackground.gameObject.SetActive(true);
